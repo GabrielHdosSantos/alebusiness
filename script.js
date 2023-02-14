0d496652a095b8a4e186e5082757606b778a4fbd
Gabriel Santos <gabrielh.folks@gmail.com>
2023-02-14 00:17:52 -0300

    contador

--- script.js
+++ script.js
@@ -2,3 +2,47 @@
 var x = setInterval(function() {
 console.log('teste');
 }, 1000);
+
+
+
+// C O U N T D O W N //
+
+// Set the date we're counting down to
+
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const d = new Date();
+
+
+var day = new Date().getDate();
+var monthName = month[d.getMonth()];
+var year  = new Date().getFullYear();
+
+var countDownDate = new Date(monthName + day + year + " 23:59:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("horas").innerHTML = hours;
+    document.getElementById("minutos").innerHTML = minutes;
+    document.getElementById("segundos").innerHTML = seconds;
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
